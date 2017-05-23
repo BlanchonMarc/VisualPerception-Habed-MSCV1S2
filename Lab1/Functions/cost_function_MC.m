8a9728052847a2e973ee87939ebdd8c83e5116c2
BlanchonMarc <fr.marc.blanchon@gmail.com>
2017-05-23 10:47:11 +0200

    Intermediate

---
+++ Lab1/Functions/cost_function_MC.m
@@ -0,0 +1,14 @@
+function cost = cost_function_MC(input_arg)
+global Fs;
+Aj = input_arg;
+
+Ai = input_arg;
+cost = 0.0;
+for i=1:size(Fs,3)
+    for j=i+1:size(Fs,4)
+        [U,S,V] = svd(Aj'*Fs(:,:,i,j)*Ai);
+        cost = cost + (S(1,1) - S(2,2)) / (S(1,1) + S(2,2));
+        %cost = cost + (S(1,1) - S(2,2)) / (S(2,2));
+    end
+end
+end
